0145ce3c927f4e30b7055ab8c92bd2b524352341
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-07-24 14:58:58 -0700

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,19 +1,23 @@
 #include "sort.h"
 /**
- * bubble_sort - sort array min to max
+ * bubble_sort - sort array from min to max
  * @array: array
  * @size: array size
  */
-void bubble_sort(int *array, size_t size) {
-  for (size_t i = 0; i < size - 1; i++) {
-    for (size_t j = 0; j < size - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        int temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-        print_array(array, size);
-      }
-      
-    }
-  }
+void bubble_sort(int *array, size_t size)
+{
+
+	size_t i, j, temp = 0;
+
+	for (i = 0; i < size; i++)
+		for (j = 0; j < size; j++)
+		{
+			if (array[j] > array[j + 1] && array[j + 1])
+			{
+			tmp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
+			print_array(array, size);
+			}
+		}
 }
