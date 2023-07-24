2e14f5fe5fb06efaa77469d891a0038fc963f62e
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-07-24 15:14:09 -0700

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,12 +7,15 @@
 void bubble_sort(int *array, size_t size)
 {
 
-	size_t i, j, temp = 0;
+	size_t i, index, tmp = 0;
+
+	if (size < 2)
+		return;
 
 	for (i = 0; i < size; i++)
 		for (j = 0; j < size; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[j] > array[j + 1] && array[j + 1])
 			{
 			tmp = array[j];
 			array[j] = array[j + 1];
