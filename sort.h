60b7e9103fd4556b0b6ca369686bb3674e3334d0
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-07-25 15:01:23 +0100

    Adding function prototypes for Task 0 in sort.h

--- sort.h
+++ sort.h
@@ -1,8 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -12,15 +13,13 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-listint_t *swap_node(listint_t *node, listint_t **list);
-void insertion_sort_list(listint_t **list);
+void bubble_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
