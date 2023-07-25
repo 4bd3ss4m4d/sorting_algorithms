3325ad7cd4a405c08d75283a329b60e8fe0b1137
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-07-25 12:31:16 -0700

    Cocktail shaker sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -4,11 +4,12 @@
  *swap_node - swap a node for his previous one
  *@node: node
  *@list: node list
- *Return: return a pointer to a node
+ *Return: return a pointer to a node which was enter it
  */
 listint_t *swap_node(listint_t *node, listint_t **list)
 {
 	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
 
 	back->next = current->next;
 	if (current->next)
